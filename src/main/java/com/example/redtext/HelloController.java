1e855a046dec9beee99993ce525c19771944b49d
Lexa <etc.21@yandex.ru>
2023-10-18 21:09:53 +0300

    ss

--- src/main/java/com/example/redtext/HelloController.java
+++ src/main/java/com/example/redtext/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.redtext;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    TextField toText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    Button upBtn;
+
+    @FXML
+    Button dwBtn;
+
+    @FXML
+    Label resString;
+
+
+
 }
--- src/main/java/com/example/redtext/ObservModel.java
+++ src/main/java/com/example/redtext/ObservModel.java
@@ -1,2 +1,18 @@
-package com.example.redtext;public class ObservModel {
+package com.example.redtext;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class ObservModel {
+
+    SimpleStringProperty s = new SimpleStringProperty();
+
+    public void addStarUp(){
+        s.set("*"+s.getValue());
+    }
+
+    public void addStarDown(){
+        s.set(s.getValue()+"*");
+    }
+
+
 }
